a12a3c618e4a38b9e4b8842d591093fc5f3fad8c
Willis Wang <wwang4292@gmail.com>
2019-08-28 05:44:56 +0000

    barebone bot

---
+++ uwubot.py
@@ -0,0 +1,26 @@
+import discord
+from uwulater import uwulate
+
+TOKEN = 'NjE2MDQwOTcxODYzNDU3ODEz.XWXJ6w.BprUgtO_EL_K0oR2sDYcHD9JtHE'  
+
+client = discord.Client()
+
+prev_msg = ''
+
+@client.event
+async def on_message(message):
+    global prev_msg
+    if message.author == client.user:
+        return
+    elif message.content.startswith('!uwu'):
+        await message.channel.send(uwulate(prev_msg))
+    else:
+        prev_msg = message.content
+        print('recorded ' + prev_msg)
+
+
+@client.event
+async def on_ready():
+    print('uwu')
+
+client.run(TOKEN)
---
+++ uwulater.py
@@ -0,0 +1,15 @@
+#Makes youw text uwu wanguage uwu
+
+def uwulate(message):
+    message = message.replace('L', 'W')
+    message = message.replace('R', 'W')
+    message = message.replace('l', 'w')
+    message = message.replace('r', 'w')
+    message = message.replace("no", "nyo")
+    message = message.replace("mo", "myo")
+    message = message.replace("No", "Nyo")
+    message = message.replace("Mo", "Myo")
+    return message + " uwu"
+    
+
+
